eecfd4b0d69fed08655f1dce3d85b1c49c47db2a
Bruno Zarth <brunozarth@gmail.com>
2025-02-26 14:13:21 -0300

    First commit - Spring initlzr base

---
+++ src/test/java/com/brunozarth/syonet/SyonetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunozarth.syonet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyonetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
